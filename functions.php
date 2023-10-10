eb9c49018b67b86f03cec8f356f83a0d1c2afb7d
Nitin Patil <pallavi@wbcomdesigns.com>
2023-10-10 11:21:01 +0530

    Managed preserving parent theme customizer settings in child theme

--- functions.php
+++ functions.php
@@ -17,12 +17,33 @@
  * @link https://codex.wordpress.org/Child_Themes
  */
 function buddyxpro_child_enqueue_styles() {
-    wp_enqueue_style( 'buddyx-pro-style' , get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'buddyx-pro-child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( 'buddyx-pro-style' ),
-        wp_get_theme()->get('Version')
-    );
+	wp_enqueue_style( 'buddyx-pro-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style(
+		'buddyx-pro-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( 'buddyx-pro-style' ),
+		wp_get_theme()->get( 'Version' )
+	);
 }
 
-add_action(  'wp_enqueue_scripts', 'buddyxpro_child_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'buddyxpro_child_enqueue_styles' );
+
+if ( get_stylesheet() !== get_template() ) {
+	add_filter(
+		'pre_update_option_theme_mods_' . get_stylesheet(),
+		function ( $value, $old_value ) {
+			update_option( 'theme_mods_' . get_template(), $value );
+			return $old_value; // prevent update to child theme mods.
+		},
+		10,
+		2
+	);
+	add_filter(
+		'pre_option_theme_mods_' . get_stylesheet(),
+		function (
+			$default_values
+		) {
+			return get_option( 'theme_mods_' . get_template(), $default_values );
+		}
+	);
+}
